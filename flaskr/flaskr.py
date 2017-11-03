ecf985e991e7f530ac565121a0ee5f07fecd4420
Mikhail <mixandoma@ya.ru>
2017-11-03 19:59:54 +0500

    db autocreation

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -24,6 +24,20 @@ def connect_db():
     return rv
 
 
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', more='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+    
+
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
