25f80c4e651e3572de6504fe0c9ca1ebe7695eeb
Mikhail <mixandoma@ya.ru>
2017-11-05 00:20:29 +0500

    mispelling

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -26,7 +26,7 @@ def connect_db():
 
 def init_db():
     db = get_db()
-    with app.open_resource('schema.sql', more='r') as f:
+    with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
 
@@ -36,7 +36,7 @@ def initdb_command():
     """Initializes the database."""
     init_db()
     print('Initialized the database.')
-    
+
 
 def get_db():
     """Opens a new database connection if there is none yet for the
